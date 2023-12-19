7d9862f709a8362cfc22ab36ccc368af19b0170b
Dk007ms <dkaushik290@gmail.com>
2023-12-19 21:23:52 +0530

    80% media done

--- script.js
+++ script.js
@@ -1,162 +1,162 @@
 var wrapper=document.querySelector(".wrapper");
-var ques1=document.querySelector(".ques1");
-var faqbtn1=document.querySelector(".faqbtn1");
-var cross1=document.querySelector(".cross1");
-var plus1=document.querySelector(".plus1");
-var faqans1=document.querySelector(".faqans1");
+// var ques1=document.querySelector(".ques1");
+// var faqbtn1=document.querySelector(".faqbtn1");
+// var cross1=document.querySelector(".cross1");
+// var plus1=document.querySelector(".plus1");
+// var faqans1=document.querySelector(".faqans1");
 
-var ques2=document.querySelector(".ques2");
-var faqbtn2=document.querySelector(".faqbtn2");
-var cross2=document.querySelector(".cross2");
-var plus2=document.querySelector(".plus2");
-var faqans2=document.querySelector(".faqans2");
+// var ques2=document.querySelector(".ques2");
+// var faqbtn2=document.querySelector(".faqbtn2");
+// var cross2=document.querySelector(".cross2");
+// var plus2=document.querySelector(".plus2");
+// var faqans2=document.querySelector(".faqans2");
 
 
-var ques3=document.querySelector(".ques3");
-var faqbtn3=document.querySelector(".faqbtn3");
-var cross3=document.querySelector(".cross3");
-var plus3=document.querySelector(".plus3");
-var faqans3=document.querySelector(".faqans3");
+// var ques3=document.querySelector(".ques3");
+// var faqbtn3=document.querySelector(".faqbtn3");
+// var cross3=document.querySelector(".cross3");
+// var plus3=document.querySelector(".plus3");
+// var faqans3=document.querySelector(".faqans3");
 
-var ques4=document.querySelector(".ques4");
-var faqbtn4=document.querySelector(".faqbtn4");
-var cross4=document.querySelector(".cross4");
-var plus4=document.querySelector(".plus4");
-var faqans4=document.querySelector(".faqans4");
+// var ques4=document.querySelector(".ques4");
+// var faqbtn4=document.querySelector(".faqbtn4");
+// var cross4=document.querySelector(".cross4");
+// var plus4=document.querySelector(".plus4");
+// var faqans4=document.querySelector(".faqans4");
 
-var ques5=document.querySelector(".ques5");
-var faqbtn5=document.querySelector(".faqbtn5");
-var cross5=document.querySelector(".cross5");
-var plus5=document.querySelector(".plus5");
-var faqans5=document.querySelector(".faqans5");
+// var ques5=document.querySelector(".ques5");
+// var faqbtn5=document.querySelector(".faqbtn5");
+// var cross5=document.querySelector(".cross5");
+// var plus5=document.querySelector(".plus5");
+// var faqans5=document.querySelector(".faqans5");
 
-faqbtn1.value=0;
-faqbtn2.value=0;
-faqbtn3.value=0;
-faqbtn3.value=0;
-faqbtn4.value=0;
-faqbtn5.value=0;
+// faqbtn1.value=0;
+// faqbtn2.value=0;
+// faqbtn3.value=0;
+// faqbtn3.value=0;
+// faqbtn4.value=0;
+// faqbtn5.value=0;
 
-function checkstate1(ques,faqbtn,cross,plus,faqans){
-   if(faqbtn.value=="0"){
-      cross.style.display="inline";
-      faqans.style.display="flex";
-      ques.style.color="#8358ff";
-      plus.style.display="none";
-      faqbtn.value=1;
-      const qbar=document.querySelector(".questions1");
-      qbar.style.height="14rem";
-      qbar.style.overflow="visible";
-      faqans.style.opacity="0.5";
-     }
-     else{
-      cross.style.display="none";
-      faqans.style.display="none";
-      ques.style.color="black";
-      plus.style.display="inline";
-      faqbtn.value=0;
-      const qbar=document.querySelector(".questions1");
-      qbar.style.height="6rem";
-      qbar.style.overflow="hidden";
-      faqans.style.opacity="0";
-   }
-}
-function checkstate2(ques,faqbtn,cross,plus,faqans){
-   if(faqbtn.value=="0"){
-      cross.style.display="inline";
-      faqans.style.display="flex";
-      ques.style.color="#8358ff";
-      plus.style.display="none";
-      faqbtn.value=1;
-      const qbar=document.querySelector(".questions2");
-      qbar.style.height="14rem";
-      qbar.style.overflow="visible";
-      faqans.style.opacity="0.5";
-     }
-     else{
-      cross.style.display="none";
-      faqans.style.display="none";
-      ques.style.color="black";
-      plus.style.display="inline";
-      faqbtn.value=0;
-      const qbar=document.querySelector(".questions2");
-      qbar.style.height="7rem";
-      qbar.style.overflow="hidden";
-      faqans.style.opacity="0";
-   }
-}
-function checkstate3(ques,faqbtn,cross,plus,faqans){
-   if(faqbtn.value=="0"){
-      cross.style.display="inline";
-      faqans.style.display="flex";
-      ques.style.color="#8358ff";
-      plus.style.display="none";
-      faqbtn.value=1;
-      const qbar=document.querySelector(".questions3");
-      qbar.style.height="12rem";
-      qbar.style.overflow="visible";
-      faqans.style.opacity="0.5";
-     }
-     else{
-      cross.style.display="none";
-      faqans.style.display="none";
-      ques.style.color="black";
-      plus.style.display="inline";
-      faqbtn.value=0;
-      const qbar=document.querySelector(".questions3");
-      qbar.style.height="7rem";
-      qbar.style.overflow="hidden";
-      faqans.style.opacity="0";
-   }
-}
-function checkstate4(ques,faqbtn,cross,plus,faqans){
-   if(faqbtn.value=="0"){
-      cross.style.display="inline";
-      faqans.style.display="flex";
-      ques.style.color="#8358ff";
-      plus.style.display="none";
-      faqbtn.value=1;
-      const qbar=document.querySelector(".questions4");
-      qbar.style.height="12rem";
-      qbar.style.overflow="visible";
-      faqans.style.opacity="0.5";
-     }
-     else{
-      cross.style.display="none";
-      faqans.style.display="none";
-      ques.style.color="black";
-      plus.style.display="inline";
-      faqbtn.value=0;
-      const qbar=document.querySelector(".questions4");
-      qbar.style.height="6rem";
-      qbar.style.overflow="hidden";
-      faqans.style.opacity="0";
-   }
-}
-function checkstate5(ques,faqbtn,cross,plus,faqans){
-   if(faqbtn.value=="0"){
-      cross.style.display="inline";
-      faqans.style.display="flex";
-      ques.style.color="#8358ff";
-      plus.style.display="none";
-      faqbtn.value=1;
-      const qbar=document.querySelector(".questions5");
-      qbar.style.height="14rem";
-      qbar.style.overflow="visible";
-      faqans.style.opacity="0.5";
-     }
-     else{
-      cross.style.display="none";
-      faqans.style.display="none";
-      ques.style.color="black";
-      plus.style.display="inline";
-      faqbtn.value=0;
-      const qbar=document.querySelector(".questions5");
-      qbar.style.height="6rem";
-      qbar.style.overflow="hidden";
-      faqans.style.opacity="0";
-   }
-}
+// function checkstate1(ques,faqbtn,cross,plus,faqans){
+//    if(faqbtn.value=="0"){
+//       cross.style.display="inline";
+//       faqans.style.display="flex";
+//       ques.style.color="#8358ff";
+//       plus.style.display="none";
+//       faqbtn.value=1;
+//       const qbar=document.querySelector(".questions1");
+//       qbar.style.height="14rem";
+//       qbar.style.overflow="visible";
+//       faqans.style.opacity="0.5";
+//      }
+//      else{
+//       cross.style.display="none";
+//       faqans.style.display="none";
+//       ques.style.color="black";
+//       plus.style.display="inline";
+//       faqbtn.value=0;
+//       const qbar=document.querySelector(".questions1");
+//       qbar.style.height="6rem";
+//       qbar.style.overflow="hidden";
+//       faqans.style.opacity="0";
+//    }
+// }
+// function checkstate2(ques,faqbtn,cross,plus,faqans){
+//    if(faqbtn.value=="0"){
+//       cross.style.display="inline";
+//       faqans.style.display="flex";
+//       ques.style.color="#8358ff";
+//       plus.style.display="none";
+//       faqbtn.value=1;
+//       const qbar=document.querySelector(".questions2");
+//       qbar.style.height="14rem";
+//       qbar.style.overflow="visible";
+//       faqans.style.opacity="0.5";
+//      }
+//      else{
+//       cross.style.display="none";
+//       faqans.style.display="none";
+//       ques.style.color="black";
+//       plus.style.display="inline";
+//       faqbtn.value=0;
+//       const qbar=document.querySelector(".questions2");
+//       qbar.style.height="7rem";
+//       qbar.style.overflow="hidden";
+//       faqans.style.opacity="0";
+//    }
+// }
+// function checkstate3(ques,faqbtn,cross,plus,faqans){
+//    if(faqbtn.value=="0"){
+//       cross.style.display="inline";
+//       faqans.style.display="flex";
+//       ques.style.color="#8358ff";
+//       plus.style.display="none";
+//       faqbtn.value=1;
+//       const qbar=document.querySelector(".questions3");
+//       qbar.style.height="12rem";
+//       qbar.style.overflow="visible";
+//       faqans.style.opacity="0.5";
+//      }
+//      else{
+//       cross.style.display="none";
+//       faqans.style.display="none";
+//       ques.style.color="black";
+//       plus.style.display="inline";
+//       faqbtn.value=0;
+//       const qbar=document.querySelector(".questions3");
+//       qbar.style.height="7rem";
+//       qbar.style.overflow="hidden";
+//       faqans.style.opacity="0";
+//    }
+// }
+// function checkstate4(ques,faqbtn,cross,plus,faqans){
+//    if(faqbtn.value=="0"){
+//       cross.style.display="inline";
+//       faqans.style.display="flex";
+//       ques.style.color="#8358ff";
+//       plus.style.display="none";
+//       faqbtn.value=1;
+//       const qbar=document.querySelector(".questions4");
+//       qbar.style.height="12rem";
+//       qbar.style.overflow="visible";
+//       faqans.style.opacity="0.5";
+//      }
+//      else{
+//       cross.style.display="none";
+//       faqans.style.display="none";
+//       ques.style.color="black";
+//       plus.style.display="inline";
+//       faqbtn.value=0;
+//       const qbar=document.querySelector(".questions4");
+//       qbar.style.height="6rem";
+//       qbar.style.overflow="hidden";
+//       faqans.style.opacity="0";
+//    }
+// }
+// function checkstate5(ques,faqbtn,cross,plus,faqans){
+//    if(faqbtn.value=="0"){
+//       cross.style.display="inline";
+//       faqans.style.display="flex";
+//       ques.style.color="#8358ff";
+//       plus.style.display="none";
+//       faqbtn.value=1;
+//       const qbar=document.querySelector(".questions5");
+//       qbar.style.height="14rem";
+//       qbar.style.overflow="visible";
+//       faqans.style.opacity="0.5";
+//      }
+//      else{
+//       cross.style.display="none";
+//       faqans.style.display="none";
+//       ques.style.color="black";
+//       plus.style.display="inline";
+//       faqbtn.value=0;
+//       const qbar=document.querySelector(".questions5");
+//       qbar.style.height="rem";
+//       qbar.style.overflow="hidden";
+//       faqans.style.opacity="0";
+//    }
+// }
 
 const header_btn = document.querySelector(".header_get_started_btn");
 const header_get_started_btn_img = document.querySelector(".header_get_started_btn_img");
@@ -373,3 +373,42 @@ newnav.addEventListener("click", () => {
   toggleimg.classList.toggle("newnavimgdisplay2"); // Replace "newnavht" with your actual class name
 });
 
+// accordian code
+
+const accordians = document.querySelectorAll('.accordian');
+const button=document.querySelector('button');
+button.value='0';
+
+accordians.forEach(accordian => {
+    const icon = accordian.querySelector('.icon');
+    const icon2 = accordian.querySelector('.icon2');
+    const answer = accordian.querySelector('.answer');
+    const h4 = accordian.querySelector('h4');
+    const button = accordian.querySelector('button');
+    button.value='0';
+
+    accordian.addEventListener('click', () => {
+        
+        if(button.value==='0'){
+            button.value='1';
+            icon.style.display="none"
+            icon2.style.display="inline-block"
+            answer.style.maxHeight = answer.scrollHeight + 'px';
+            h4.style.color="#8c56ff";
+            console.log('entry1');
+        }
+
+        else{
+            button.value='0';
+            icon.style.display="inline-block"
+            icon2.style.display="none";
+            answer.style.maxHeight = null;
+            h4.style.color="black";
+            console.log('entry2')
+        }
+       
+        
+    })
+})
+
+// accordian code ends
